21933d474fd8fa943622d77e32952553a0b1748f
choi-yong-jo <54878504+choi-yong-jo@users.noreply.github.com>
2024-03-27 23:29:25 +0900

    Add files via upload

---
+++ sptek/src/main/java/com/sptek/demo/sample/HelloController.java
@@ -0,0 +1,15 @@
+package com.sptek.demo.sample;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 1. 원격 프로그램으로 등록
+@RestController
+public class HelloController {
+    // 2. URL과 메서드를 연결
+    @RequestMapping("/hello")
+    public String main() {
+        System.out.println("Hello");
+        return "Hello";
+    }
+}
