379b60632144a6ca08dcf143889f7545096adb96
cyjo1 <cyjo1@LAPTOP-RG4AELKM>
2024-03-27 23:25:13 +0900

    commit

---
+++ src/main/java/com/sptek/demo/SptekApplication.java
@@ -0,0 +1,13 @@
+package com.sptek.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptekApplication.class, args);
+	}
+
+}
